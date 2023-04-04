aa6ec846c94a4f4ea421b7dc51ae69614365bab5
mayukh2002 <dasmayukh2002@gmail.com>
2023-04-04 15:09:15 -0700

    Box Component coming together
    I got the box component started, and things are starting to get interesting.

---
+++ src/lib/components/Box.svelte
@@ -0,0 +1,15 @@
+<script>
+
+//Put some logic here regarding the type of box
+//1. Blank  2. Tile 3. Default
+
+ export let values = ["",false,false,false];
+
+</script>
+
+<div class="relative flex items-center justify-center w-20 h-20 border-black border-2">
+    <img class ="absolute left-0 top-0 h-5" src="diagArrow.svg" alt="diag">
+    <img class ="absolute left-auto top-0 h-5" src="upArrow.svg" alt="up">
+    <img class ="absolute left-0 top-auto h-5" src="leftArrow.svg" alt="left">
+    <h1 class="text-center text-2xl">{values[0]}</h1>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <!-- <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<h1 class="text-3xl font-bold underline italic">
+
+<!-- <h1 class=" text-cyan-400 text-3xl font-bold underline italic">
     Hello world!
 </h1>
 <button class="btn">Button</button>
@@ -9,4 +10,122 @@
     :global(html) {
       background-color: theme(colors.gray.100);
     }
-  </style>
+  </style> -->
+
+
+  <script>
+    import Box from "$lib/components/Box.svelte";
+import { each } from "svelte/internal";
+    const array = [[, 0], [-1, 3]]   
+
+    let headers = "If you do what you've always done you'll get what you've always gotten";
+    const words = headers.split(' ');
+
+    let seqA = '';
+    let seqB = '';
+    let renderTable = true;
+
+    function initializeMatrix(s1,s2){
+      /**
+         * @type {any[]}
+         */
+      let mat = [];
+      for(let row = 0; row < s1.length + 2; row++){
+        /**
+           * @type {any[]}
+           */
+        let arr = [];
+        for(let col = 0; col < s2.length + 2; col++){
+        
+        }
+        mat.append(arr);
+      }
+      return mat;
+    }
+
+    function needlemanWunsch(s1, s2){
+      alert("alignmnet algo triggered!");
+      // /**
+      //    * @type {never[]}
+      //    */
+      // let mat = [];
+      // for(let i: i < s2.length; i++){
+
+      // }
+      // return mat;
+    }
+
+  </script>
+
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">What is your first nucleotide sequence?</span>
+  </label>
+  <input bind:value={seqA} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <label class="label">
+  </label>seqA = {seqA}
+</div>
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">What is your second nucleotide sequence?</span>
+  </label>
+  <input bind:value={seqB} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <label class="label">
+  </label>seqB = {seqB}
+</div>
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Match Score</span>
+  </label>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <label class="label">
+  </label>
+</div>
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Mismatch Score</span>
+  </label>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <label class="label">
+  </label>
+</div>
+<div class="form-control w-full max-w-xs">
+  <label class="label">
+    <span class="label-text">Gap Score</span>
+  </label>
+  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <label class="label">
+  </label>
+</div>
+
+<button  class="btn btn-wide">Compute Optimal Alignment</button>
+
+  <table>
+    {#each words as w}
+    <th class=" border-black border-4">{w}</th>
+    {/each}
+
+    {#each array as r}
+    <tr>
+      {#each r as val}
+        <td>{val}</td>
+      {/each}
+    </tr>
+    {/each}
+  </table>
+
+
+
+  {#if renderTable}
+  <table>
+    {#each array as r}
+    <tr>
+      {#each r as val}
+        <td>{val}</td>
+      {/each}
+    </tr>
+    {/each}
+  </table>
+  {/if}
+
+  <Box values={["42",false,false,false]}/>

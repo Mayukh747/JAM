26e45027a222af6e0a87ca35a77c2c89cad879e8
mayukh2002 <dasmayukh2002@gmail.com>
2023-04-11 14:27:13 -0700

    Animation + Alignment Calculations
    there's a long way to go on Needleman Wunsch, but I think I'm ready to expand my horizons into what will become the 'real deal' of this project!

---
+++ src/lib/components/Table.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Box from "./Box.svelte";
+
+    export let table = [[]];
+    
+</script>
+
+<table>
+    {#each table as r}
+    <tr>
+      {#each r as v}
+        <td><Box values={v}/></td>
+      {/each}
+    </tr>
+
+    {/each}
+  </table>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,96 +1,92 @@
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
 
-<!-- <h1 class=" text-cyan-400 text-3xl font-bold underline italic">
-    Hello world!
-</h1>
-<button class="btn">Button</button>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
+<script>
+  import Box from "$lib/components/Box.svelte";
+    import Table from "$lib/components/Table.svelte";
+  import { sequence } from "@sveltejs/kit/hooks";
+  import { each } from "svelte/internal";
+  const array = [[, 0], [-1, 3]]   
+
+  let headers = "If you do what you've always done you'll get what you've always gotten";
+  const words = headers.split(' ');
+
+  let seqA = 'duck';
+  let seqB = 'trump';
+  let match = 1;
+  let mismatch = -1;
+  let gap = -2;
+  let renderTable = true;
+  /**
+   * @type {any[]}
+   */
+  let table = [];
+
+  /**
+   * @param {string} s1
+   * @param {string} s2
+   */
+  function initializeMatrix(s1, s2){
+
+    table = Array.from({ length: s1.length+2 }, () => Array.from({ length: s2.length + 2 }, () => ["",false,false,false]));
+
+    table[1][1][0] = '0';
+    let val = 0;
+    for(let col = 0; col < s2.length; col++){
+      table[0][col+2][0] = s2.charAt(col);
+      val-=2;
+      table[1][col+2][0] = val.toString();
     }
-  </style> -->
+    val = 0;
+    for(let row = 0; row < s1.length; row++){
+      table[row+2][0][0] = s1.charAt(row);
+      val-=2
+      table[row+2][1][0] = val.toString();
+    }
 
+    //To make the top left 3 square visible
+    table[0][0][0] = " ";
+    table[0][1][0] = " ";
+    table[1][0][0] = " ";
 
-  <script>
-    import Box from "$lib/components/Box.svelte";
-    import { sequence } from "@sveltejs/kit/hooks";
-    import { each } from "svelte/internal";
-    const array = [[, 0], [-1, 3]]   
+  }
 
-    let headers = "If you do what you've always done you'll get what you've always gotten";
-    const words = headers.split(' ');
+  /**
+   * @param {string} s1
+   * @param {string} s2
+   */
+  async function needlemanWunsch(s1, s2){
+    initializeMatrix(s1,s2);
 
-    let seqA = '';
-    let seqB = '';
-    let renderTable = true;
-    /**
-     * @type {any[]}
-     */
-    let table = [];
-
-    /**
-     * @param {string} s1
-     * @param {string} s2
-     */
-    function initializeMatrix(s1, s2){
-
-      table = Array.from({ length: s1.length+2 }, () => Array.from({ length: s2.length + 2 }, () => ["",false,false, false]));
-
-      table[1][1][0] = '0';
-      let val = 0;
-      for(let col = 0; col < s2.length; col++){
-        table[0][col+2][0] = s2.charAt(col);
-        val-=2;
-        table[1][col+2][0] = val.toString();
+    for(let idx = 2; idx < Math.min(table.length, table[0].length); idx++){
+      //Row
+      for(let col = idx; col < table[0].length; col++){
+        solveCellNeedlemanWunsch(idx, col);
+        await new Promise(r => setTimeout(r, 100));
       }
-      val = 0;
-      for(let row = 0; row < s1.length; row++){
-        table[row+2][0][0] = s1.charAt(row);
-        val-=2
-        table[row+2][1][0] = val.toString();
-      }
+      //Col
+      for(let row = idx + 1; row < table.length; row++){
+        solveCellNeedlemanWunsch(row, idx);
+        await new Promise(r => setTimeout(r, 100));
+      } 
     }
+  }
 
-    /**
-     * @param {string} s1
-     * @param {string} s2
-     */
-    function needlemanWunsch(s1, s2){
-      initializeMatrix(s1,s2);
+  /**
+   * @param {number} row
+   * @param {number} col
+   */
+  function solveCellNeedlemanWunsch(row, col){
+    // console.log(gap + " " + typeof(gap));
+    let topVal = Number(table[row-1][col][0]) + Number(gap);
+    let diagVal = Number(table[row-1][col-1][0]) + (table[row][0][0]==table[0][col][0] ? Number(match) : Number(mismatch));
+    let leftVal = Number(table[row][col-1][0]) + Number(gap);
+    
+    let cellScore = Math.max(topVal, diagVal, leftVal);
 
-      for(let idx = 2; idx < Math.min(table.length, table[0].length); idx++){
-        //Row
-        for(let col = idx; col < table[0].length; col++){
-          solveCellNeedlemanWunsch(idx, col);
-          // table[idx][col] = ["3", false, false, false];
-        }
-        //Col
-        for(let row = idx + 1; row < table.length; row++){
-          solveCellNeedlemanWunsch(row, idx);
-          // table[row][idx] = ["2", false, false, false];
-        }
-        
-      }
-    }
-
-    /**
-     * @param {number} row
-     * @param {number} col
-     */
-    function solveCellNeedlemanWunsch(row, col){
-      let topVal = table[row-1][col][0];
-      let diagVal = table[row-1][col-1][0];
-      let leftVal = table[row][col-1][0];
-
-      let minVal = Math.min(topVal, diagVal, leftVal);
-
-      table[row][col] = [minVal.toString(), topVal==minVal, diagVal==minVal, leftVal==minVal];
-    }
+    table[row][col] = [cellScore.toString(), topVal==cellScore, diagVal==cellScore, leftVal==cellScore];
+  }
 
 
-  </script>
+</script>
 
   <!-- {@debug table} -->
 
@@ -102,6 +98,8 @@
   <label class="label">
   </label>seqA = {seqA}
 </div>
+
+
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text">What is your second nucleotide sequence?</span>
@@ -110,33 +108,53 @@
   <label class="label">
   </label>seqB = {seqB}
 </div>
+
+
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text">Match Score</span>
   </label>
-  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <input bind:value={match} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
   <label class="label">
   </label>
+  match = {match}
 </div>
+
+
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text">Mismatch Score</span>
   </label>
-  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <input bind:value={mismatch} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
   <label class="label">
   </label>
+  mismatch = {mismatch}
 </div>
+
+
 <div class="form-control w-full max-w-xs">
   <label class="label">
     <span class="label-text">Gap Score</span>
   </label>
-  <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+  <input bind:value={gap} type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
   <label class="label">
   </label>
+  gap = {gap}
 </div>
 
 <button  on:click={() => needlemanWunsch(seqA,seqB)} class="btn btn-wide">Compute Optimal Alignment</button>
 
+
+<br>
+<!-- bruh this slider gay -->
+<input type="range" min="0" max="100" value="25" class="range" step="25" />
+<div class="w-full flex justify-between text-xs px-2">
+  <span>|</span>
+  <span>|</span>
+  <span>|</span>
+  <span>|</span>
+  <span>|</span>
+</div>
 <!-- <button  on:click={() => needlemanWunsch(seqA, seqB)} class="btn btn-wide">Compute Optimal Alignment</button> -->
 
   <!-- <table>
@@ -168,7 +186,7 @@
   {/if} -->
 
 
-  <table>
+  <!-- <table>
     {#each table as r}
     <tr>
       {#each r as v}
@@ -177,7 +195,8 @@
     </tr>
 
     {/each}
-  </table>
+  </table> -->
+  <Table table={table}/>
   
 
   <!-- <Box values={["42",false,false,false]}/> -->
